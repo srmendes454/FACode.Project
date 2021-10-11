c00eabf388017f3a0fadce1c1bcb28e030622755
Rafael Mendes <sr.mendes1809@gmail.com>
2021-10-11 19:29:48 -0300

    Add project files.

---
+++ FACode.Project.Context/Domain/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace FACode.Project.Context.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Value { get; set; }
+        public int Inventory { get; set; }
+    }
+}
---
+++ FACode.Project.Context/Domain/InfraData/DataBaseContext/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using FACode.Project.Context.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace FACode.Project.Context.Domain.InfraData.DataBaseContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .Build();
+
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("ServerConnection"));
+        }
+    }
+}
